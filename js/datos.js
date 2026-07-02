165bf129fd3f00be4a668d2ef942a03f46d84e9f
sanchezjaider481-lang <sanchezjaider481@gmail.com>
2026-07-02 01:41:20 -0400

    Update datos.js

--- js/datos.js
+++ js/datos.js
@@ -1,31 +1,4 @@
 const misPartidos = [
-    { 
-        id: 1, 
-        campeonato: "CHAMPIONS LEAGUE", 
-        local: "Real Madrid", 
-        visitante: "Man. City", 
-        cuota1: 2.10, 
-        cuotaX: 3.40, 
-        cuota2: 3.10 
-    },
-    { 
-        id: 2, 
-        campeonato: "PREMIER LEAGUE", 
-        local: "Liverpool", 
-        visitante: "Arsenal", 
-        cuota1: 1.95, 
-        cuotaX: 3.50, 
-        cuota2: 3.80 
-    }
+    { campeonato: "CHAMPIONS", local: "Real Madrid", visitante: "Man. City", hora: "14:00", cuota1: 2.10, cuotaX: 3.40, cuota2: 3.10 }
 ];
-
-// Función para calcular Parlay automáticamente
-function calcularParlay(cuotasSeleccionadas, monto) {
-    // Multiplica todas las cuotas seleccionadas
-    const cuotaTotal = cuotasSeleccionadas.reduce((total, cuota) => total * cuota, 1);
-    return {
-        cuotaFinal: cuotaTotal.toFixed(2),
-        ganancia: (monto * cuotaTotal).toFixed(2)
-    };
-}
 
