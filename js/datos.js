98497a7e2bf2650615302b5309291e0c4940d8b1
sanchezjaider481-lang <sanchezjaider481@gmail.com>
2026-07-02 00:19:15 -0400

    Update datos.js

--- js/datos.js
+++ js/datos.js
@@ -1,3 +1,4 @@
 const misPartidos = [
-    { campeonato: "CHAMPIONS LEAGUE", local: "Real Madrid", visitante: "Manchester City" }
+    { campeonato: "CHAMPIONS LEAGUE", local: "Real Madrid", visitante: "Manchester City", cuota1: "2.10", cuotaX: "3.40", cuota2: "3.10" },
+    { campeonato: "PREMIER LEAGUE", local: "Liverpool", visitante: "Arsenal", cuota1: "1.95", cuotaX: "3.50", cuota2: "3.80" }
 ];
